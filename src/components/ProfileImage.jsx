e95b031da118acfa4f8db7b6bd8c5e8558b282ce
Elliot Fernandez <elliot@hispantic.com>
2023-08-21 15:17:44 +0100

    lab finalizado

---
+++ src/components/ProfileImage.jsx
@@ -0,0 +1,13 @@
+function ProfileImage(props) {
+console.log(props.image)
+    return (
+        <img
+        src={props.image}
+        className="profile"
+        alt="profile"
+      />
+    )
+
+}
+
+export default ProfileImage;
